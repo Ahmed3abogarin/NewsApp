9c4332bb84bb4956dc5e52856b600145c9d3862f
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-14 00:33:37 +0300

    .

--- app/src/main/java/com/ahmed/newsapp/util/Dimens.kt
+++
@@ -1,9 +0,0 @@
-package com.ahmed.newsapp.util
-
-import androidx.compose.ui.unit.dp
-
-object Dimens {
-    val MediumPadding1 = 8.dp
-    val MediumPadding2 = 8.dp
-    val IndicatorSize = 4.dp
-}
