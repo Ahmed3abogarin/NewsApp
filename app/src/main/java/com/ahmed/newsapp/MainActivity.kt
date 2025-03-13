a5761f922b5e9772c455d4d9f3bba56b3f0b5214
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-13 17:25:54 +0300

    init

--- app/src/main/java/com/ahmed/newsapp/MainActivity.kt
+++ app/src/main/java/com/ahmed/newsapp/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
-        installSplashScreen()
         setContent {
             NewsAppTheme {
 
