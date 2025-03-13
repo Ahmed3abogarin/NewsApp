55388843a9b3616a3489437d9d78d55a287488f0
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-13 22:30:51 +0300

    .

---
+++ app/src/main/java/com/ahmed/newsapp/manager/LocalUserManager.kt
@@ -0,0 +1,11 @@
+package com.ahmed.newsapp.manager
+
+import kotlinx.coroutines.flow.Flow
+
+interface LocalUserManager {
+
+    // save the app entry
+    suspend fun saveAppEntry()
+
+    fun readAppEntry(): Flow<Boolean>
+}
