88788a03b316cd7d1b11239d55a32a926ceec821
Ahmed Adil <ahmedadilabogarin@gmail.com>
2025-03-13 22:15:02 +0300

    init

---
+++ app/src/main/java/com/ahmed/newsapp/Test.kt
@@ -0,0 +1,15 @@
+package com.ahmed.newsapp
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Greet(){
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Text(text = "Test")
+    }
+}
